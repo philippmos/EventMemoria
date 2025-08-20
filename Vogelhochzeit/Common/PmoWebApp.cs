98d55688512715503c7e91df4089ac5c19df6e49
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2025-08-20 22:29:15 +0200

    Refactor app startup and update UI components

---
+++ Vogelhochzeit/Common/PmoWebApp.cs
@@ -0,0 +1,40 @@
+﻿using MudBlazor.Services;
+using Vogelhochzeit.Components;
+
+namespace Vogelhochzeit.Common;
+
+public static class PmoWebApp
+{
+    public static WebApplicationBuilder CreateBuilder(params string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddMudServices();
+
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        return builder;
+    }
+
+    public static WebApplication BuildWebApp(this WebApplicationBuilder builder)
+    {
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        return app;
+    }
+}
