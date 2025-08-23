037384e066dcd6a70d727284a65a9fd7743e44cf
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2025-08-23 09:57:52 +0200

    Add customizable page title to Home page

--- Vogelhochzeit/Common/Settings/CustomizationOptions.cs
+++ Vogelhochzeit/Common/Settings/CustomizationOptions.cs
@@ -2,6 +2,7 @@ namespace Vogelhochzeit.Common.Settings;
 
 public record CustomizationOptions
 {
+    public string PageTitle { get; init; } = string.Empty;
     public string Title { get; init; } = string.Empty;
     public string AvatarImagePath { get; init; } = string.Empty;
     public string Names { get; init; } = string.Empty;
