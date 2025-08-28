fbbfc9c68394baf3d5209a07e1086680783715e9
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2025-08-28 21:33:38 +0200

    Setup QR Code Feature

--- src/EventMemoria.Web/Common/Settings/CustomizationOptions.cs
+++ src/EventMemoria.Web/Common/Settings/CustomizationOptions.cs
@@ -7,6 +7,7 @@ public record CustomizationOptions
     public string AvatarPath { get; init; } = string.Empty;
     public string Names { get; init; } = string.Empty;
     public string WelcomeMessage { get; init; } = string.Empty;
+    public string QrCodeTargetUrl { get; init; } = string.Empty;
     public NameModalOptions NameModal { get; init; } = new();
 }
 
