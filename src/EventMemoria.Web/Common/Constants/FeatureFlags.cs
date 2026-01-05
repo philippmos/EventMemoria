eca6e08f47be7b89ff89fc76cb425ea6ec5fc1b2
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2026-01-05 18:08:29 +0100

    Rename Downloads to Subscription

--- src/EventMemoria.Web/Common/Constants/FeatureFlags.cs
+++ src/EventMemoria.Web/Common/Constants/FeatureFlags.cs
@@ -2,7 +2,7 @@ namespace EventMemoria.Web.Common.Constants;
 
 public static class FeatureFlags
 {
-    public const string EnableDownloadPage = nameof(EnableDownloadPage);
+    public const string EnableSubscribePage = nameof(EnableSubscribePage);
     public const string EnableQrCodePage = nameof(EnableQrCodePage);
     public const string EnablePhotoUpload = nameof(EnablePhotoUpload);
     public const string ShowAuthorInfo = nameof(ShowAuthorInfo);
